d97afafa237892dd01dc6b6252d637781441c175
Sheharyar Abid <iamsheharyarabid@gmail.com>
2024-07-05 18:55:33 +0500

    modified:   src/app/app.component.ts 	modified:   src/app/components/tasks-item/tasks-item.component.html 	modified:   src/app/components/tasks-item/tasks-item.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButtonComponent, TasksComponent, FontAwesomeModule],
+  imports: [RouterOutlet, HeaderComponent, ButtonComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
 }
--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './tasks-item.component.html',
   styleUrl: './tasks-item.component.css'
 })
 export class TasksItemComponent implements OnInit{
   @Input() task! :Task;  //the '!' disables strick initialization  for task
-
+  faTimes = faTimes;
   constructor() {
 
   }
