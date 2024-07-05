bbbfda2af665bf9b661848230849db4306f92875
Sheharyar Abid <iamsheharyarabid@gmail.com>
2024-07-05 18:11:45 +0500

    new file:   src/app/Task.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	new file:   src/app/components/tasks-item/tasks-item.component.css 	new file:   src/app/components/tasks-item/tasks-item.component.html 	new file:   src/app/components/tasks-item/tasks-item.component.spec.ts 	new file:   src/app/components/tasks-item/tasks-item.component.ts 	new file:   src/app/components/tasks/tasks.component.css 	new file:   src/app/components/tasks/tasks.component.html 	new file:   src/app/components/tasks/tasks.component.spec.ts 	new file:   src/app/components/tasks/tasks.component.ts 	new file:   src/app/mock-task.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ButtonComponent],
+  imports: [RouterOutlet, HeaderComponent, ButtonComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tasks-item.component.html',
+  styleUrl: './tasks-item.component.css'
+})
+export class TasksItemComponent implements OnInit{
+  @Input() task! :Task;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
